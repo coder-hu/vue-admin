0c8d11cad0e87a0c4ebc5b92c66fc7a6c3f29a34
huxianbao <wb-hxb265612@alibaba-inc.com>
2018-04-16 15:02:29 +0800

    2018-4-16

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ body {
 	padding: 0px;
 	/*background: url(assets/bg1.jpg) center !important;
 		background-size: cover;*/
-	// background: #1F2D3D;
+	/*background: #1F2D3D;*/
 	font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
 	font-size: 14px;
 	-webkit-font-smoothing: antialiased;
